52feab69f1e130808663033138f1ab284c000acb
jaskaran-01 <59540918+jaskaran-01@users.noreply.github.com>
2023-11-05 07:13:41 +0530

    starting main prep for db conn

--- lib/DBConn/DBConn.cpp
+++ lib/DBConn/DBConn.cpp
@@ -5,13 +5,44 @@ DBConn::DBConn() {
 }
 
 bool DBConn::connectToDatabase() {
-  // Implement your code to connect to the database here
+  // Initialize the database connection
+  if (database.connect("server_address", "username", "password")) {
+    // Connection successful
+    return true;
+  } else {
+    // Connection failed
+    return false;
+  }
 }
 
 bool DBConn::checkInPerson(const String &uid) {
-  // Implement your code to check in a person in the database
+  // Construct an SQL query to update the CheckedIn and CheckinDateTime fields for the specified UID
+  String query = "UPDATE people SET CheckedIn = true, CheckinDateTime = NOW() WHERE UID = '" + uid + "'";
+
+  // Execute the query
+  if (database.execute(query)) {
+    // Update successful
+    return true;
+  } else {
+    // Update failed
+    return false;
+  }
 }
 
 bool DBConn::getPersonInfo(const String &uid, String &name, bool &checkedIn, String &checkinDateTime) {
-  // Implement your code to retrieve a person's info from the database
+  // Construct an SQL query to retrieve information for the specified UID
+  String query = "SELECT Name, CheckedIn, CheckinDateTime FROM people WHERE UID = '" + uid + "'";
+
+  // Execute the query and retrieve the results
+  if (database.execute(query)) {
+    if (database.resultsAvailable()) {
+      // Fetch the results
+      name = database.getString(0);
+      checkedIn = database.getBoolean(1);
+      checkinDateTime = database.getString(2);
+      return true;
+    }
+  }
+  // Query or data retrieval failed
+  return false;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,17 @@
-void setup(){}
-void loop(){}
+#include <DBConn.h>
+
+DBConn db;
+
+void setup() {
+  Serial.begin(9600);
+  if (db.connectToDatabase()) {
+    Serial.println("Connected to the database.");
+    // Use other DBConn methods here
+  } else {
+    Serial.println("Failed to connect to the database.");
+  }
+}
+
+void loop() {
+  // Your main program loop
+}
